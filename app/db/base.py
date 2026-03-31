e99d235d1a7d4eb5db99931c449e1a854e8e75cb
SukanyaSolase <134798619+SukanyaSolase@users.noreply.github.com>
2026-03-30 18:30:54 -0700

    Uncomment model imports in base.py

--- app/db/base.py
+++ app/db/base.py
@@ -4,5 +4,5 @@ class Base(DeclarativeBase):
     pass
 
 # IMPORTANT: import models so Alembic can discover them
-# from app.models.user import User  # noqa: F401
-# from app.models.application import Application  # noqa: F401
+from app.models.user import User  # noqa: F401
+from app.models.application import Application  # noqa: F401
